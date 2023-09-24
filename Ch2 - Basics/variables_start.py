eeba1db7b69d3a81f16026fbabcb3ffcf9de7658
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-24 21:13:40 +0000

    Creating some examples thru using slices

--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -13,21 +13,26 @@ mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+#print(myint)
+#print(myfloat)
+#print(mystr)
+#print(mybool)
+#print(mylist)
+#print(mytuple)
+#print(mydict)
 
 # re-declaring a variable works
+myint = "abc"
+print(myint)
 
 # to access a member of a sequence type, use []
-
+print(mylist[2])
+print(mytuple[1])
 # use slices to get parts of a sequence
-
+print(mylist[1:5])
+print(mylist[1:5:2])
 # you can use slices to reverse a sequence
+print(mylist[::-1])
 
 # dictionaries are accessed via keys
 
