e547bcb51b2c857f5b3578c9684577e952d51791
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-24 21:04:14 +0000

    Create Hello World and make it a function

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,15 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("Hello World!")
+    name = input("What is your name?")
+    print("Nice to meet you", name)
 
+
+#This tells Python to look for the function above
+#This Python function should run as a main function
+#Also it tells Python that this is a standalone program
+#And not a module or chunk of code to be exported
+if __name__ == "__main__":
+    main()
