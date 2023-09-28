70f907761be37f5600e1322ede8625f03045a6d3
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-28 20:30:48 +0000

    Updated comments regarding calling a function

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -23,6 +23,6 @@ def func1():
 
 
 #Function execution examples (wrong and right)
-func1() #prints I am a function
-print(func1()) #prints I am a function
-print(func1) #Returns none
+func1() #prints I am a function from direct function call
+print(func1()) #prints I am a function  and also prints out the return value of NONE 
+print(func1) #Function definition is being printed where the object resides in memory
