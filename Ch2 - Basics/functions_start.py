ef8596e3f7286da1bcb234867d5ac16e11d629a0
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-28 20:26:13 +0000

    First TODO of basic function and function call.

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -5,6 +5,9 @@
 
 
 # TODO: define a basic function
+#Functions start with the word def followed by the function name and parameters in parentheses and then a colon
+def func1():
+    print("I am a function") # this is the body of the function.
 
 
 # TODO: function that takes arguments
@@ -19,3 +22,7 @@
 # TODO: function with variable number of arguments
 
 
+#Function execution examples (wrong and right)
+func1()
+print(func1())
+print(func1)
