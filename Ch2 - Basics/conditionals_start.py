d9c041f26dd30d9204d46f259ba287a47affc0f5
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-29 20:09:44 +0000

    Putting in code to for one line conditional statement

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -6,21 +6,25 @@
 
 
 def main():
-    x, y = 100, 100   
+    x, y = 1000, 100   
 
     # conditional flow uses if, elif, else
 
-    if x < y:  #Compare x above is less than y  
-        result = "x is less than y"
-    #What is they are the same?
-    elif x == y:
-        result = "x and y are the same"
-    #Now lets put in an else statement to handle when X is greater than Y.
-    else: 
-        result = "x is greater than y"
-    print(result)
+    # if x < y:  #Compare x above is less than y  
+    #     result = "x is less than y"
+    # #What is they are the same?
+    # elif x == y:
+    #     result = "x and y are the same"
+    # #Now lets put in an else statement to handle when X is greater than Y.
+    # else: 
+    #     result = "x is greater than y"
+    # print(result)
 
     # conditional statements let you use "a if C else b"
+    
+    #Same as above but in one line and minus the ELIF.
+    result = "x is less than y" if x < y else "x is greater or equal to y"
+    print(result)
 
     # match-case makes it easy to compare multiple values
     value = "one"
