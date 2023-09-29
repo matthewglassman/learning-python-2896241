3353477c53205b5a1dfc33d46063360b02ba48cf
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-29 20:02:29 +0000

    Adding Else Statement for X > Y

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -6,12 +6,15 @@
 
 
 def main():
-    x, y = 1000, 100   #Causes and error because X will no longer be less than Y and so variable 'result' never gets created and thus print can't print anything.
+    x, y = 1000, 100   
 
     # conditional flow uses if, elif, else
 
     if x < y:  #Compare x above is less than y  
         result = "x is less than y"
+    #Now lets put in an else statement to handle when X is greater than Y.
+    else: 
+        result = "x is greater than y"
     print(result)
 
     # conditional statements let you use "a if C else b"
