7c4c91d999cf7370957d2f64312811bf59efedf9
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-29 19:57:43 +0000

    First conditional statement with X being less than Y

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -10,6 +10,10 @@ def main():
 
     # conditional flow uses if, elif, else
 
+    if x < y:  #Compare x above is less than y  
+        result = "x is less than y"
+    print(result)
+
     # conditional statements let you use "a if C else b"
 
     # match-case makes it easy to compare multiple values
