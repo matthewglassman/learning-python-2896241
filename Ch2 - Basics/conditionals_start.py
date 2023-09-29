9d9a4e6ccae9802e652bea3491d8f3251f43e941
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-29 20:15:12 +0000

    Filling in code for a Match Case example

--- Ch2 - Basics/conditionals_start.py
+++ Ch2 - Basics/conditionals_start.py
@@ -23,11 +23,21 @@ def main():
     # conditional statements let you use "a if C else b"
     
     #Same as above but in one line and minus the ELIF.
-    result = "x is less than y" if x < y else "x is greater or equal to y"
-    print(result)
+    # result = "x is less than y" if x < y else "x is greater or equal to y"
+    # print(result)
 
     # match-case makes it easy to compare multiple values
-    value = "one"
+    value = "fifteen"
+    match value:
+        case "one":
+            result = 1
+        case "two":
+            result = 2
+        case "three" | "four":  #Handles multiple possible values so 3 or 4
+            result = (3,4)
+        case _: #handles anything else
+            result = -1
+    print(result)
 
 if __name__ == "__main__":
     main()
