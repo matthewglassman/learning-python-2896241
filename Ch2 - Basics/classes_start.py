af71de40bb3a4660013e8b1146a45c2c0fd802d8
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-30 19:09:24 +0000

    Created a base class for a Vehicle

--- Ch2 - Basics/classes_start.py
+++ Ch2 - Basics/classes_start.py
@@ -5,4 +5,8 @@
 
 #Lets build an application that manages vehicles
 
+#By convention all of the methods in a class take the reference to the object as the first parameter.
 class Vehicle():
+    def __init__(self,bodystyle): #function/method is what Python calls when the object has been created and it is time to initialize the object's data
+        self.bodystyle = bodystyle #define a property on the class that will take what is equal to the bodystyle parameter that gets passed in.
+        
