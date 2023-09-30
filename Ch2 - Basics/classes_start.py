bfc73e2fdbd4aacd8af518cf0feaf58a0c47213d
Matthew Glassman <matthewglassman78@gmail.com>
2023-09-30 19:19:31 +0000

    Created a class for a Vehicle that is of class car and bodytype car

--- Ch2 - Basics/classes_start.py
+++ Ch2 - Basics/classes_start.py
@@ -9,4 +9,14 @@
 class Vehicle():
     def __init__(self,bodystyle): #function/method is what Python calls when the object has been created and it is time to initialize the object's data
         self.bodystyle = bodystyle #define a property on the class that will take what is equal to the bodystyle parameter that gets passed in.
-        
+
+#Now we can create other classes from the base class of Vehicle
+
+class Car(Vehicle):  #The item in the parantheses here is saying that Class Car inherits from Class Vehicle
+    def __init__(self, enginetype):
+        super().__init__("Car") #Keyword super initializes the super class (in this case Vehicle) and call it's init method and the bodystyle parameter ("Car")
+        # Now lets set some other properties for the Class Car
+        self.wheels = 4
+        self.doors = 4
+        self.enginetype = enginetype
+    
