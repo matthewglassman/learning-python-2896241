7cf717bca492b1116cd255c211df18f8485ca958
Matthew Glassman <matthewglassman78@gmail.com>
2023-10-05 19:24:06 +0000

    Looking at the Pi constant from the Math Module

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -9,6 +9,6 @@ import math
 print("The square root of 16 is", math.sqrt(16))
 
 # TODO: in addition to functions, some modules contain useful constants 
-
+print("Pi is,",math.pi)
 
 # TODO: try some of the math functions for yourself here:
