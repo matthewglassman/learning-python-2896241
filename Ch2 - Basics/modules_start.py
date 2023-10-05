4e45b73c104806f3fa98092d3dabbe27782bf27b
Matthew Glassman <matthewglassman78@gmail.com>
2023-10-05 19:22:59 +0000

    Use the square root function that comes with the math module

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -6,7 +6,7 @@
 import math
 
 # TODO: the math module contains lots of pre-built functions
-
+print("The square root of 16 is", math.sqrt(16))
 
 # TODO: in addition to functions, some modules contain useful constants 
 
