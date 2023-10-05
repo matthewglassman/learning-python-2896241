1200175173eb48c398e52387f2d53cccba48b660
Matthew Glassman <matthewglassman78@gmail.com>
2023-10-05 19:33:58 +0000

    Handle more than one exception

--- Ch2 - Basics/exceptions_start.py
+++ Ch2 - Basics/exceptions_start.py
@@ -8,10 +8,23 @@
 #x = 10 / 0
 # TODO: Exceptions provide a way of catching errors and then handling them in 
 # a separate section of the code to group them together
-try:
-    x = 10 /0
-except:
-    print("Well that didn't work. You can't divide by 0")
+# try:
+#     x = 10 /0
+# except:
+#     print("Well that didn't work. You can't divide by 0")
+
 
 # TODO: You can also catch specific exceptions
 
+#the following can cause multiple errors (divide by 0, not a number, etc). Can write multiple exceptions
+try:
+    answer = input("What should I divide 10 by?")
+    num = int(answer) #take input and convert to an integer and store in num variable
+    print(10/num)
+except ZeroDivisionError as e:
+    print("You can't divide by 0")
+except ValueError as e:
+    print("You didn't give me a valid number!")
+    print(e)
+finally: #Contains code that will always run. Can be used to clean up resources that were allocated like open files.
+    print("This code always runs")
