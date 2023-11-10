11f79768f0e21212752919a4f2e8b798d74f5210
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-10 21:08:07 +0000

    Print path to file and also create a tuple by splitting path and file

--- Ch3 - Files/ospathutils_start.py
+++ Ch3 - Files/ospathutils_start.py
@@ -16,10 +16,12 @@ def main():
     
     # Check for item existence and type
     print ("Item exists:", str(path.exists("textfile.txt")))
-    print("Item is a file:", path.isfile("textfile.txt")) #Checks to see if item is a file and prints this line OR
-    print("Items is a directory", path.isdir("textfile.txt")) #Checks to see if item is a directory and prints this line.
+    print("Item is a file:", path.isfile("textfile.txt")) #Checks to see if item is a file and prints this line as True or False
+    print("Items is a directory", path.isdir("textfile.txt")) #Checks to see if item is a directory and prints this line as True or False
     
     # Work with file paths
+    print("Item's path:", path.realpath("textfile.txt")) #Prints full path and filename
+    print("Item's path and name:", path.split(path.realpath("textfile.txt"))) #creates path and separately the filename as a tuple
 
     
     # Get the modification time
