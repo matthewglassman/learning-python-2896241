be0d033b71ad25faa597976794f62e07bbc6e89d
Matthew Glassman <matthewglassman78@gmail.com>
2023-11-10 20:51:39 +0000

    Now open the text file and read it and if the mode is correct print to screen.

--- Ch3 - Files/files_start.py
+++ Ch3 - Files/files_start.py
@@ -9,18 +9,22 @@ def main():
     #myfile = open("textfile.txt", "w+")
     
     # Open the file for appending text to the end
-    myfile = open("textfile.txt", "a+")
+    #myfile = open("textfile.txt", "a+")
 
     # write some lines of data to the file
-    for i in range(10):
-        myfile.write("This is some new text\n")
+    # for i in range(10):
+    #     myfile.write("This is some new text\n")
     
-    # close the file when done
-    myfile.close()
+    # # close the file when done
+    # myfile.close()
 
     
     # Open the file back up and read the contents
-
+    myfile = open("textfile.txt", "r")
+    #Check to see if the mode is correctly set to read and if so store the contents in a variable and print them to screen.
+    if myfile.mode == 'r':
+        contents = myfile.read()
+        print(contents)
     
 if __name__ == "__main__":
     main()
