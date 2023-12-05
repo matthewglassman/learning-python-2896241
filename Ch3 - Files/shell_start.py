29f981f21775402467f3009773dfa7e17eb8b454
Matthew Glassman <matthewglassman78@gmail.com>
2023-12-05 21:06:11 +0000

    Using shutil to create a copy of an existing file.

--- Ch3 - Files/shell_start.py
+++ Ch3 - Files/shell_start.py
@@ -11,11 +11,16 @@ import shutil
 
 def main():
     # make a duplicate of an existing file
-    if path.exists("textfile.txt"):
+    if path.exists("textfile.txt"):  #to make sure the file exists
         # get the path to the file in the current directory
-        
+        src = path.realpath("textfilie.txt")
         # let's make a backup copy by appending "bak" to the name
-        
+        #first lets create the destination of the path and file by concatenating the path and the file that we want to copy
+        destination = src + ".bak"
+
+        #Next we will use the copy function of the shutil module to copy the source to the destination
+        shutil.copy(src,destination)
+
         # rename the original file
         
         # now put things into a ZIP archive
