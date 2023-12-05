9ce3fad45cce6c793046b9eb864168ce51036307
Matthew Glassman <matthewglassman78@gmail.com>
2023-12-05 21:08:24 +0000

    Edited the name of the file in the src assignment line.

--- Ch3 - Files/shell_start.py
+++ Ch3 - Files/shell_start.py
@@ -13,7 +13,7 @@ def main():
     # make a duplicate of an existing file
     if path.exists("textfile.txt"):  #to make sure the file exists
         # get the path to the file in the current directory
-        src = path.realpath("textfilie.txt")
+        src = path.realpath("textfile.txt")
         # let's make a backup copy by appending "bak" to the name
         #first lets create the destination of the path and file by concatenating the path and the file that we want to copy
         destination = src + ".bak"
